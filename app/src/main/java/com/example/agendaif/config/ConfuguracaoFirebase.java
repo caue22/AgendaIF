b65251dbdeee0220742832472e23b5484e2c690f
Caue Garcia <cauegarcian@gmail.com>
2023-06-11 10:07:15 -0300

    Limpando codigos

--- app/src/main/java/com/example/agendaif/config/ConfuguracaoFirebase.java
+++ app/src/main/java/com/example/agendaif/config/ConfuguracaoFirebase.java
@@ -6,11 +6,14 @@ import com.google.firebase.database.FirebaseDatabase;
 public class ConfuguracaoFirebase {
     private static DatabaseReference database;
 
-
+    // Retorna a instância do FirebaseDatabase
     public static DatabaseReference getFirebaseDatabase() {
-        if(database==null){
+        // Verifica se a referência do database é nula
+        if (database == null) {
+            // Obtém a instância do FirebaseDatabase e a referência para o nó raiz do banco de dados
             database = FirebaseDatabase.getInstance().getReference();
         }
+        // Retorna a referência do database
         return database;
     }
 }
